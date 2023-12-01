f482e269f5cc706aca419af19f34470eac776656
cesarombredane <combredane@gmail.com>
2023-12-01 18:29:23 +0100

    feat: user detail page

--- src/app/[lang]/admin/[userId]/page.tsx
+++ src/app/[lang]/admin/[userId]/page.tsx
@@ -3,6 +3,8 @@ import { getUser } from "@actions/getUsers";
 import { isAdmin } from "@actions/isAdmin";
 import { auth } from "@lib/auth";
 import { getDictionnary, type Locale } from "@lib/getDictionnary";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@ui/accordion";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui/tabs";
 
 export default async function AdminUserPage({ params }: { params: { lang: string; userId: string } }) {
@@ -12,50 +14,7 @@ export default async function AdminUserPage({ params }: { params: { lang: string
 
   const dictionnary = await getDictionnary(params.lang as Locale);
 
-  // const user1 = await getUser(parseInt(params.userId));
-  // console.log(user1);
-
-  const user = {
-    id: params.userId,
-    email: "test@test.fr",
-    easy: 90,
-    medium: 90,
-    hard: 90,
-    games: [
-      {
-        id: 1,
-        progress: -1,
-        answers: [
-          {
-            id: 1,
-            statement: "voici la question",
-            answer: "voici la réponse",
-            difficulty: "easy",
-            grade: 90,
-          },
-          {
-            id: 2,
-            statement: "voici la question",
-            answer: "voici la réponse",
-            difficulty: "easy",
-            grade: 90,
-          },
-          {
-            id: 3,
-            statement: "voici la question",
-            answer: "voici la réponse",
-            difficulty: "easy",
-            grade: 90,
-          },
-        ],
-      },
-      {
-        id: 2,
-        progress: 4,
-        answers: [],
-      },
-    ],
-  };
+  const user = await getUser(parseInt(params.userId));
 
   return (
     <>
@@ -63,38 +22,102 @@ export default async function AdminUserPage({ params }: { params: { lang: string
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">{user.email} :</h2>
-            <h5 className="m-4 text-xl text-green-600">{user.easy}%</h5>
-            <h5 className="m-4 text-xl text-yellow-600">{user.medium}%</h5>
-            <h5 className="m-4 text-xl text-red-600">{user.hard}%</h5>
           </div>
-          <Tabs defaultValue={user.games[0].id.toString()} className="space-y-4">
-            <TabsList>
-              {user.games.map((game) => (
-                <TabsTrigger key={game.id} value={game.id.toString()}>
-                  {game.id}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-            {user.games.map((game) => (
-              <TabsContent key={game.id} value={game.id.toString()} className="space-y-4">
-                {game.progress !== -1 ? (
-                  <div className="items-center">
-                    <div className="text-xl font-medium">In progress...</div>
-                    <div className="text-xl font-medium">question: {game.progress}</div>
-                  </div>
-                ) : (
-                  game.answers.map((answer) => (
-                    <div key={answer.id} className="flex flex-row items-center justify-between space-y-0 pb-2">
-                      <div className="text-sm font-medium">{answer.statement}</div>
-                      <div className="text-sm font-medium">{answer.answer}</div>
-                      <div className="text-sm font-medium">{answer.difficulty}</div>
-                      <div className="text-sm font-medium">{answer.grade}</div>
-                    </div>
-                  ))
-                )}
-              </TabsContent>
-            ))}
-          </Tabs>
+          {user.games && (
+            <div className="flex-1 space-y-4 p-8 pt-6">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">{dictionnary.admin.game_nb}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">{user.games.length}</div>
+                    <p className="text-xs text-muted-foreground">{dictionnary.admin.game_played}</p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">{dictionnary.admin.easy}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold text-green-600">{user.easy}%</div>
+                    <p className="text-xs text-muted-foreground">{dictionnary.admin.easy_desc}</p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">{dictionnary.admin.medium}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold text-yellow-600">{user.medium}%</div>
+                    <p className="text-xs text-muted-foreground">{dictionnary.admin.medium_desc}</p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">{dictionnary.admin.hard}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold text-red-600">{user.hard}%</div>
+                    <p className="text-xs text-muted-foreground">{dictionnary.admin.hard_desc}</p>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="text-2xl font-bold">All games :</div>
+              <Tabs defaultValue={user.games[0].id.toString()} className="space-y-4">
+                <TabsList>
+                  {user.games.map((game, idx) => (
+                    <TabsTrigger key={game.id} value={game.id.toString()}>
+                      {idx + 1}
+                    </TabsTrigger>
+                  ))}
+                </TabsList>
+                {user.games.map((game) => (
+                  <TabsContent key={game.id} value={game.id.toString()} className="space-y-4">
+                    {game.progress !== -1 ? (
+                      <div className="items-center">
+                        <div className="text-xl font-medium">In progress...</div>
+                        <div className="text-xl font-medium">question: {game.progress}</div>
+                      </div>
+                    ) : (
+                      <Accordion key={game.id} type="single" collapsible>
+                        {game.answers?.map((answer, idx) => (
+                          <AccordionItem value={answer.id.toString()} key={answer.id}>
+                            <AccordionTrigger className="flex flex-row items-center justify-between space-y-0 pb-2">
+                              <div className="text-sm font-medium">
+                                Question {idx + 1} ({answer.difficulty})
+                              </div>
+                            </AccordionTrigger>
+                            <AccordionContent>
+                              <div className="grid grid-cols-2 gap-4">
+                                <Card>
+                                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">Statement :</CardTitle>
+                                  </CardHeader>
+                                  <CardContent>
+                                    <p className="text-xs text-muted-foreground">{answer.statement}</p>
+                                  </CardContent>
+                                </Card>
+                                <Card>
+                                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">Answer :</CardTitle>
+                                  </CardHeader>
+                                  <CardContent>
+                                    <p className="text-xs text-muted-foreground">{answer.answer}</p>
+                                  </CardContent>
+                                  <CardFooter>Grade : {answer.grade}</CardFooter>
+                                </Card>
+                              </div>
+                            </AccordionContent>
+                          </AccordionItem>
+                        ))}
+                      </Accordion>
+                    )}
+                  </TabsContent>
+                ))}
+              </Tabs>
+            </div>
+          )}
         </div>
       </main>
     </>
