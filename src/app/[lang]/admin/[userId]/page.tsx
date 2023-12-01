284732f5e2dcf22073e67e953d375ee29727a6df
cesarombredane <combredane@gmail.com>
2023-12-01 19:08:52 +0100

    fix: lang in user detail

--- src/app/[lang]/admin/[userId]/page.tsx
+++ src/app/[lang]/admin/[userId]/page.tsx
@@ -63,7 +63,7 @@ export default async function AdminUserPage({ params }: { params: { lang: string
                   </CardContent>
                 </Card>
               </div>
-              <div className="text-2xl font-bold">All games :</div>
+              <div className="text-2xl font-bold">{dictionnary.admin.user_detail.all_games}</div>
               <Tabs defaultValue={user.games[0].id.toString()} className="space-y-4">
                 <TabsList>
                   {user.games.map((game, idx) => (
@@ -76,8 +76,10 @@ export default async function AdminUserPage({ params }: { params: { lang: string
                   <TabsContent key={game.id} value={game.id.toString()} className="space-y-4">
                     {game.progress !== -1 ? (
                       <div className="items-center">
-                        <div className="text-xl font-medium">In progress...</div>
-                        <div className="text-xl font-medium">question: {game.progress}</div>
+                        <div className="text-xl font-medium">{dictionnary.admin.user_detail.in_progress}</div>
+                        <div className="text-xl font-medium">
+                          {dictionnary.admin.user_detail.question + game.progress}
+                        </div>
                       </div>
                     ) : (
                       <Accordion key={game.id} type="single" collapsible>
@@ -85,14 +87,16 @@ export default async function AdminUserPage({ params }: { params: { lang: string
                           <AccordionItem value={answer.id.toString()} key={answer.id}>
                             <AccordionTrigger className="flex flex-row items-center justify-between space-y-0 pb-2">
                               <div className="text-sm font-medium">
-                                Question {idx + 1} ({answer.difficulty})
+                                {dictionnary.admin.user_detail.question + (idx + 1)} ({answer.difficulty})
                               </div>
                             </AccordionTrigger>
                             <AccordionContent>
                               <div className="grid grid-cols-2 gap-4">
                                 <Card>
                                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Statement :</CardTitle>
+                                    <CardTitle className="text-sm font-medium">
+                                      {dictionnary.admin.user_detail.statement}
+                                    </CardTitle>
                                   </CardHeader>
                                   <CardContent>
                                     <p className="text-xs text-muted-foreground">{answer.statement}</p>
@@ -100,12 +104,14 @@ export default async function AdminUserPage({ params }: { params: { lang: string
                                 </Card>
                                 <Card>
                                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Answer :</CardTitle>
+                                    <CardTitle className="text-sm font-medium">
+                                      {dictionnary.admin.user_detail.answer}
+                                    </CardTitle>
                                   </CardHeader>
                                   <CardContent>
                                     <p className="text-xs text-muted-foreground">{answer.answer}</p>
                                   </CardContent>
-                                  <CardFooter>Grade : {answer.grade}</CardFooter>
+                                  <CardFooter>{dictionnary.admin.user_detail.grade + answer.grade}</CardFooter>
                                 </Card>
                               </div>
                             </AccordionContent>
