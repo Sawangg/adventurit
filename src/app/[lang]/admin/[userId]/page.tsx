010372245db88e4b66ae268b2b8e34a7b9856348
cesarombredane <combredane@gmail.com>
2023-12-01 14:20:12 +0100

    feat: user admin 1/?

--- src/app/[lang]/admin/[userId]/page.tsx
+++ src/app/[lang]/admin/[userId]/page.tsx
@@ -1,7 +1,102 @@
-export default function AdminUserPage() {
+import { redirect } from "next/navigation";
+import { getUser } from "@actions/getUsers";
+import { isAdmin } from "@actions/isAdmin";
+import { auth } from "@lib/auth";
+import { getDictionnary, type Locale } from "@lib/getDictionnary";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui/tabs";
+
+export default async function AdminUserPage({ params }: { params: { lang: string; userId: string } }) {
+  const session = await auth();
+  const admin = await isAdmin(session!.user!.email!);
+  if (!admin) return redirect("/");
+
+  const dictionnary = await getDictionnary(params.lang as Locale);
+
+  // const user1 = await getUser(parseInt(params.userId));
+  // console.log(user1);
+
+  const user = {
+    id: params.userId,
+    email: "test@test.fr",
+    easy: 90,
+    medium: 90,
+    hard: 90,
+    games: [
+      {
+        id: 1,
+        progress: -1,
+        answers: [
+          {
+            id: 1,
+            statement: "voici la question",
+            answer: "voici la réponse",
+            difficulty: "easy",
+            grade: 90,
+          },
+          {
+            id: 2,
+            statement: "voici la question",
+            answer: "voici la réponse",
+            difficulty: "easy",
+            grade: 90,
+          },
+          {
+            id: 3,
+            statement: "voici la question",
+            answer: "voici la réponse",
+            difficulty: "easy",
+            grade: 90,
+          },
+        ],
+      },
+      {
+        id: 2,
+        progress: 4,
+        answers: [],
+      },
+    ],
+  };
+
   return (
     <>
-      <main className="p-4"></main>
+      <main className="p-4">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">{user.email} :</h2>
+            <h5 className="m-4 text-xl text-green-600">{user.easy}%</h5>
+            <h5 className="m-4 text-xl text-yellow-600">{user.medium}%</h5>
+            <h5 className="m-4 text-xl text-red-600">{user.hard}%</h5>
+          </div>
+          <Tabs defaultValue={user.games[0].id.toString()} className="space-y-4">
+            <TabsList>
+              {user.games.map((game) => (
+                <TabsTrigger key={game.id} value={game.id.toString()}>
+                  {game.id}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            {user.games.map((game) => (
+              <TabsContent key={game.id} value={game.id.toString()} className="space-y-4">
+                {game.progress !== -1 ? (
+                  <div className="items-center">
+                    <div className="text-xl font-medium">In progress...</div>
+                    <div className="text-xl font-medium">question: {game.progress}</div>
+                  </div>
+                ) : (
+                  game.answers.map((answer) => (
+                    <div key={answer.id} className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <div className="text-sm font-medium">{answer.statement}</div>
+                      <div className="text-sm font-medium">{answer.answer}</div>
+                      <div className="text-sm font-medium">{answer.difficulty}</div>
+                      <div className="text-sm font-medium">{answer.grade}</div>
+                    </div>
+                  ))
+                )}
+              </TabsContent>
+            ))}
+          </Tabs>
+        </div>
+      </main>
     </>
   );
 }
--- src/app/_actions/getUsers.ts
+++ src/app/_actions/getUsers.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { db } from "@db/index";
 import { answers, games, questions, users } from "@db/schema";
 
+// get all users for admin page
 const UserGradesSchema = z.object({
   id: z.number(),
   email: z.string().email({ message: "invalid email" }),
@@ -34,6 +35,93 @@ export const getAllUsers = async (): Promise<AllUsersGrades> => {
   const requestResult = await preparedUserGradesRequest.execute();
   if (!requestResult || requestResult.length === 0) throw new Error("No users found");
   const result = AllUsersGradesSchema.safeParse(requestResult);
+  if (!result.success) throw new Error(result.error.message);
+  return result.data;
+};
+
+// get one user for admin detail page
+const UserSchema = z.object({
+  id: z.number(),
+  email: z.string().email({ message: "invalid email" }),
+  easy: z.number(),
+  medium: z.number(),
+  hard: z.number(),
+  games: z
+    .array(
+      z.object({
+        id: z.string(),
+        progress: z.number(),
+        answers: z
+          .array(
+            z.object({
+              id: z.number(),
+              statement: z.string().nullable(),
+              answer: z.string(),
+              difficulty: z.string().nullable(),
+              grade: z.number().nullable(),
+            }),
+          )
+          .optional(),
+      }),
+    )
+    .optional(),
+});
+export type User = z.infer<typeof UserSchema>;
+
+const preparedUserRequest = db
+  .select({
+    id: users.id,
+    email: users.email,
+    easy: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"easy"} THEN ${answers.grade} END), 0) AS INT)`,
+    medium: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"medium"} THEN ${answers.grade}  END), 0) AS INT)`,
+    hard: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"hard"} THEN ${answers.grade} END), 0) AS INT)`,
+  })
+  .from(users)
+  .leftJoin(games, eq(users.id, games.userId))
+  .leftJoin(answers, eq(games.id, answers.gameId))
+  .leftJoin(questions, eq(questions.id, answers.questionId))
+  .where(eq(users.id, sql.placeholder("id")))
+  .groupBy(users.id)
+  .prepare("preparedUser");
+
+const preparedUserGamesRequest = db
+  .select({
+    id: games.id,
+    progress: games.progress,
+  })
+  .from(games)
+  .where(eq(games.userId, sql.placeholder("id")))
+  .prepare("preparedUserGames");
+
+const preparedUserAnswersRequest = db
+  .select({
+    id: answers.id,
+    statement: questions.statement,
+    answer: answers.answer,
+    difficulty: questions.difficulty,
+    grade: answers.grade,
+  })
+  .from(answers)
+  .leftJoin(questions, eq(questions.id, answers.questionId))
+  .where(eq(answers.gameId, sql.placeholder("gameId")))
+  .prepare("preparedUserAnswers");
+
+export const getUser = async (id: number): Promise<User> => {
+  const requestUserResult = await preparedUserRequest.execute({ id });
+  if (!requestUserResult || requestUserResult.length === 0) throw new Error("User not found");
+  const user: User = requestUserResult[0];
+
+  const requestGamesResult = await preparedUserGamesRequest.execute({ id });
+  if (!requestGamesResult) throw new Error("User games not found");
+  user.games = requestGamesResult;
+
+  for (const game of user.games) {
+    const requestAnswersResult = await preparedUserAnswersRequest.execute({ gameId: game.id });
+    if (!requestAnswersResult) throw new Error("User answers not found");
+    game.answers = requestAnswersResult;
+  }
+
+  const result = UserSchema.safeParse(user);
   if (!result.success) throw new Error(result.error.message);
   return result.data;
 };
