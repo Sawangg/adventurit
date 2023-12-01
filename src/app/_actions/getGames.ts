01cf6f4d9b24dceb1ac494be38ce1d0d559eab91
cesarombredane <combredane@gmail.com>
2023-12-01 17:06:53 +0100

    fix: separate prepare statement and request

--- src/app/_actions/getGames.ts
+++ src/app/_actions/getGames.ts
@@ -1,21 +1,12 @@
 "use server";
 
-import { sql } from "drizzle-orm";
 import { z } from "zod";
-import { db } from "@db/index";
-import { games } from "@db/schema";
+import { preparedGameNumberRequest } from "@db/prepared/gameNumber";
 
 const GameNumberSchema = z.object({
   gameNumber: z.number(),
 });
 const AllGameNumberSchema = z.array(GameNumberSchema);
-
-const preparedGameNumberRequest = db
-  .select({
-    gameNumber: sql<number>`CAST(COUNT(${games.id}) AS INT)`,
-  })
-  .from(games)
-  .prepare("preparedGameNumber");
 
 export const getGameNumber = async (): Promise<number> => {
   const requestResult = await preparedGameNumberRequest.execute();
--- src/app/_actions/getUsers.ts
+++ src/app/_actions/getUsers.ts
@@ -1,9 +1,10 @@
 "use server";
 
-import { eq, sql } from "drizzle-orm";
 import { z } from "zod";
-import { db } from "@db/index";
-import { answers, games, questions, users } from "@db/schema";
+import { preparedUserAnswersRequest } from "@db/prepared/userAnswers";
+import { preparedUserGamesRequest } from "@db/prepared/userGames";
+import { preparedUserGradesRequest } from "@db/prepared/userGrades";
+import { preparedUsersGradesRequest } from "@db/prepared/usersGrades";
 
 // get all users for admin page
 const UserGradesSchema = z.object({
@@ -16,23 +17,8 @@ const UserGradesSchema = z.object({
 const AllUsersGradesSchema = z.array(UserGradesSchema);
 export type AllUsersGrades = z.infer<typeof AllUsersGradesSchema>;
 
-const preparedUserGradesRequest = db
-  .select({
-    id: users.id,
-    email: users.email,
-    easy: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"easy"} THEN ${answers.grade} END), 0) AS INT)`,
-    medium: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"medium"} THEN ${answers.grade}  END), 0) AS INT)`,
-    hard: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"hard"} THEN ${answers.grade} END), 0) AS INT)`,
-  })
-  .from(users)
-  .leftJoin(games, eq(users.id, games.userId))
-  .leftJoin(answers, eq(games.id, answers.gameId))
-  .leftJoin(questions, eq(questions.id, answers.questionId))
-  .groupBy(users.id)
-  .prepare("preparedUserGrades");
-
 export const getAllUsers = async (): Promise<AllUsersGrades> => {
-  const requestResult = await preparedUserGradesRequest.execute();
+  const requestResult = await preparedUsersGradesRequest.execute();
   if (!requestResult || requestResult.length === 0) throw new Error("No users found");
   const result = AllUsersGradesSchema.safeParse(requestResult);
   if (!result.success) throw new Error(result.error.message);
@@ -68,46 +54,8 @@ const UserSchema = z.object({
 });
 export type User = z.infer<typeof UserSchema>;
 
-const preparedUserRequest = db
-  .select({
-    id: users.id,
-    email: users.email,
-    easy: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"easy"} THEN ${answers.grade} END), 0) AS INT)`,
-    medium: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"medium"} THEN ${answers.grade}  END), 0) AS INT)`,
-    hard: sql<number>`CAST(COALESCE(AVG(CASE WHEN ${questions.difficulty} = ${"hard"} THEN ${answers.grade} END), 0) AS INT)`,
-  })
-  .from(users)
-  .leftJoin(games, eq(users.id, games.userId))
-  .leftJoin(answers, eq(games.id, answers.gameId))
-  .leftJoin(questions, eq(questions.id, answers.questionId))
-  .where(eq(users.id, sql.placeholder("id")))
-  .groupBy(users.id)
-  .prepare("preparedUser");
-
-const preparedUserGamesRequest = db
-  .select({
-    id: games.id,
-    progress: games.progress,
-  })
-  .from(games)
-  .where(eq(games.userId, sql.placeholder("id")))
-  .prepare("preparedUserGames");
-
-const preparedUserAnswersRequest = db
-  .select({
-    id: answers.id,
-    statement: questions.statement,
-    answer: answers.answer,
-    difficulty: questions.difficulty,
-    grade: answers.grade,
-  })
-  .from(answers)
-  .leftJoin(questions, eq(questions.id, answers.questionId))
-  .where(eq(answers.gameId, sql.placeholder("gameId")))
-  .prepare("preparedUserAnswers");
-
 export const getUser = async (id: number): Promise<User> => {
-  const requestUserResult = await preparedUserRequest.execute({ id });
+  const requestUserResult = await preparedUserGradesRequest.execute({ id });
   if (!requestUserResult || requestUserResult.length === 0) throw new Error("User not found");
   const user: User = requestUserResult[0];
 
