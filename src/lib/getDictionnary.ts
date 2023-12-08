59e652952c1d06c789bb424b22f5decfe8383766
Sawangg <le1112osee@gmail.com>
2023-12-08 12:40:13 +0100

    fix: remove ts 5.3 syntax not compatible with prettier

--- src/lib/getDictionnary.ts
+++ src/lib/getDictionnary.ts
@@ -1,9 +1,8 @@
 import "server-only";
 
 const dictionnaries = {
-  // eslint-disable-next-line prettier/prettier
-  en: () => import("@public/locales/en.json", { with: { type: "json" }}).then((module) => module.default),
-  fr: () => import("@public/locales/fr.json", { with: { type: "json" }}).then((module) => module.default),
+  en: () => import("@public/locales/en.json").then((module) => module.default),
+  fr: () => import("@public/locales/fr.json").then((module) => module.default),
 };
 
 export type Locale = keyof typeof dictionnaries;
