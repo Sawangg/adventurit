3b932cfb0ded57497e8c74a028163dd0732076be
Léo MERCIER <38500427+Sawangg@users.noreply.github.com>
2023-12-13 19:39:37 +0100

    feat: Main menu improvement (#65)
    * feat: better main menu
    
    * fix: getDictionary

--- src/lib/getDictionnary.ts
+++ src/lib/getDictionnary.ts
@@ -8,4 +8,7 @@ const dictionnaries = {
 export type Locale = keyof typeof dictionnaries;
 export type Dictionnary = Awaited<ReturnType<typeof getDictionnary>>;
 
-export const getDictionnary = async <T extends Locale>(locale: T) => await dictionnaries[locale]();
+export const getDictionnary = async <T extends Locale>(locale: T) => {
+  if (typeof dictionnaries[locale] === "function") return dictionnaries[locale]();
+  throw new Error(`Locale ${locale} not found`);
+};
