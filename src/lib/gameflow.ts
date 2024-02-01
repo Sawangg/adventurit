70ce404b8255245b1ac4c125ba6254f26014a696
Sawangg <le1112osee@gmail.com>
2024-02-01 18:54:27 +0100

    fix(gameplay): added punctuation

--- src/lib/gameflow.ts
+++ src/lib/gameflow.ts
@@ -5,25 +5,25 @@ export const gameflow = (add: (commands: Command[]) => void, element: string) =>
   switch (element) {
     case "phare":
       add([
-        { type: "dialog", args: ["Answer the next question"] },
+        { type: "dialog", args: ["Answer the next question."] },
         {
           type: "question",
           args: [
-            "What is the Python code to check if a number is even",
+            "What is the Python code to check if a number is even ?",
             ["1) is_even = num % 2 == 1", "2) is_even = num % 2 == 0"],
           ],
         },
-        { type: "dialog", args: ["Good job! Now go to the church for your next question"] },
+        { type: "dialog", args: ["Good job ! Now go to the church for your next question !"] },
       ]);
       break;
     case "big_island_eglise":
       add([
-        { type: "dialog", args: ["I hope you're ready!"] },
+        { type: "dialog", args: ["I hope you're ready !"] },
         {
           type: "coding",
-          args: ["What is the Python code to check if a number is even", "is_even = num % 2 == 0"],
+          args: ["What is the Python code to check if a number is even ?", "is_even = num % 2 == 0"],
         },
-        { type: "dialog", args: ["Wow! You've earned it! Here is your badge"] },
+        { type: "dialog", args: ["Wow ! You've earned it ! Here is your badge !"] },
       ]);
       break;
     default:
